51dae9ebb95c49108d1f5874a1d4b6fd3fcb76cd
qjkramos01 <qjkramos01@tip.edu.ph>
2025-11-14 22:58:34 +0800

    app.py

--- app.py
+++ app.py
@@ -3,14 +3,11 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-def load_image_to_rgb(file):
-    # Open image
+def load_image_to_gray(file):
     img = Image.open(file)
     
-    # Force convert to RGB (3 channels)
-    img = img.convert("RGB")
+    img = img.convert("L")
     
-    # Convert to NumPy array
     img_array = np.array(img)
     
     return img_array
@@ -23,13 +20,14 @@ model = tf.keras.models.load_model("fish_dog_cnn_model.h5")
 uploaded = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded:
-    img_array = load_image_to_rgb(uploaded)
+    img_array = load_image_to_gray(uploaded)
 
     img_resized = Image.fromarray(img_array).resize((128,128))
     img_arr = np.array(img_resized).astype("float32") / 255.0
-    img_arr = img_arr.reshape(1,128,128,3)
 
-    st.image(img_resized, width=300, caption="Uploaded Image")
+    img_arr = img_arr.reshape(1,128,128,1)
+
+    st.image(img_resized, width=300, caption="Uploaded Image", clamp=True)
 
     prediction = model.predict(img_arr)[0][0]
     
@@ -41,4 +39,3 @@ if uploaded:
     st.subheader("Prediction:")
     st.success(label)
     st.write(f"Model confidence: {prediction:.4f}")
-
